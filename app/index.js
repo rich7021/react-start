961b3b95b84dee2405d9c91296b4e544bc32a41d
rich7021 <coconutfu@gmail.com>
2017-11-07 01:03:33 +0800

    add some to "about" page

--- app/index.js
+++ app/index.js
@@ -1,13 +1,11 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-import { Router, Route, hashHistory, IndexRoute } from "react-router";
 
 require("./css/main.css");
 
 //TODO how to make it as requrie?
 import App from "./pages/App";
-import About from "./pages/About";
-import HeroPage from "./pages/HeroPage"
+import HeroPage from "./pages/HeroPage";
 
 {
   ReactDOM.render(<App />, document.getElementById("header"));
--- app/pages/App.js
+++ app/pages/App.js
@@ -1,11 +1,11 @@
-
 import React, { Component } from "react";
 import ReactDOM from 'react-dom'
 
-import HeaderBtn from "../Components/HeaderBtn";
 import HeroPage from "./HeroPage";
 import About from "./About";
 import Artist from "./Artist";
+
+import HeaderBtn from "../Components/HeaderBtn";
 
 class App extends React.Component {
   renderContainer(obj) {
@@ -17,20 +17,15 @@ class App extends React.Component {
       <div>
         <div id="logo">
           <div className="col-4">
-            <img id="logo-img" src={require("../images/logo.jpg")} alt="花享" />
-          </div>{" "}
-        </div>{" "}
+            <img id="logo-img" src={require("../images/logo.jpg")} alt="花享" onClick={() => this.renderContainer(<HeroPage />)} />
+          </div>
+        </div>
         <div id="nav">
           <HeaderBtn value="About" onClick={() => this.renderContainer(<About />)} />
           <HeaderBtn value="Artist" onClick={() => this.renderContainer(<Artist />)}/>
           <HeaderBtn value="Product" />
           <HeaderBtn value="booking" />
-          {/* if not using JSX*/}{" "}
-          {/*{React.createElement(HeaderBtn, {value:"about"})}
-                {React.createElement(HeaderBtn, {value:"product"})}
-                {React.createElement(HeaderBtn, {value:"beautician"})}
-                {React.createElement(HeaderBtn, {value:"booking"})}*/}{" "}
-        </div>{" "}
+        </div>
       </div>
     );
   }
