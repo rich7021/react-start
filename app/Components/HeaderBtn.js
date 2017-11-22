615f5975dceeb9b91ea306af77e6cd6a267b3781
rich7021 <coconutfu@gmail.com>
2017-11-23 00:34:03 +0800

    add font-family and navbtn hover border

--- app/Components/HeaderBtn.js
+++ app/Components/HeaderBtn.js
@@ -4,7 +4,9 @@ class HeaderBtn extends React.Component {
   render() {
     return (
       <div className="col-2 nav-btn">
-        <p onClick={this.props.onClick}>{this.props.value}</p>
+        <p>
+          <span onClick={this.props.onClick}>{this.props.value}</span>
+        </p>
       </div>
     );
   }
