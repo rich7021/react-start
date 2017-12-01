7e7cadc8727f8326d5e3989c5f8fc58bb0fb8d3f
rich7021 <coconutfu@gmail.com>
2017-12-02 00:55:50 +0800

    heroPage done

--- app/pages/HeroPage.js
+++ app/pages/HeroPage.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 
 class HeroPage extends React.Component {
   render() {
+    let heroPageData = {
+      img: "url(" + require("../images/HeroPage.jpg") + ")"
+    };
     return (
       <div className="container">
-        <div id="hero-img-container">
-          <img
-            id="hero-img"
-            src={require("../images/HeroPage.jpg")}
-            alt="img"
-          />
+        <div
+          id="hero-img-container"
+          style={{ backgroundImage: heroPageData.img }}
+        >
+          <div id="hero-intro-container">
+            <h2>
+              花享。花想<br />將帶你一起走入這寧靜多彩的境地
+            </h2>
+          </div>
         </div>
       </div>
     );
