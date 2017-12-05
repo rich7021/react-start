d907221a1d1051da460bf72c390b32f4fdffc482
rich7021 <coconutfu@gmail.com>
2017-12-05 14:49:30 +0800

    heropage portfolio template

--- app/pages/HeroPage.js
+++ app/pages/HeroPage.js
@@ -8,8 +8,14 @@ class HeroPage extends React.Component {
     };
     let intro = heroPageData.intro.split(", ");
 
+    let portfolioItems = [];
+    for (let i = 0; i < 4; i++) {
+      portfolioItems.push(require("../images/logo.jpg"));
+    }
+
     return [
       <div
+        key={1}
         id="hero-img-container"
         className="container"
         style={{ backgroundImage: heroPageData.img }}
@@ -20,7 +26,7 @@ class HeroPage extends React.Component {
           })}
         </div>
       </div>,
-      <div className="container">
+      <div key={2} className="container">
         <div className="container-banner">
           <h1>About</h1>
         </div>
@@ -28,7 +34,7 @@ class HeroPage extends React.Component {
           <div id="about-img-div">
             <img
               id="about-img"
-              className={this.props.className}
+              className="circle-image"
               src={require("../images/logo.jpg")}
               alt="花享"
               onClick={() => this.props.renderBody(<HeroPage />)}
@@ -42,6 +48,26 @@ class HeroPage extends React.Component {
             />j32897vajho9dshf
           </div>
         </span>
+      </div>,
+      <div key={3} className="container" id="heropage-portfolio">
+        <div className="container-banner">
+          <h1>Portfolio</h1>
+        </div>
+        <div className="col-12 container-box">
+          {portfolioItems.map((item, i) => {
+            return (
+              <span key={i} className="col-3 portfolio-span">
+                <img
+                  id="portfolio-img"
+                  className="circle-image"
+                  src={item}
+                  alt="花享"
+                  onClick={() => this.props.renderBody(<HeroPage />)}
+                />
+              </span>
+            );
+          })}
+        </div>
       </div>
     ];
   }
