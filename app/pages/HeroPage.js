4f12e7022d94ad8c346ce0270415c343ec79879e
rich7021 <coconutfu@gmail.com>
2017-12-05 16:32:56 +0800

    heropage portfolio template

--- app/pages/HeroPage.js
+++ app/pages/HeroPage.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 
 class HeroPage extends React.Component {
   render() {
+    return [<HeroPage_Home />, <HeroPage_About />, <HeroPage_Portfolio />];
+  }
+}
+
+class HeroPage_Home extends React.Component {
+  render() {
     let heroPageData = {
       img: "url(" + require("../images/HeroPage.jpg") + ")",
       intro: "花享。花想, 將帶你一起走入這寧靜多彩的境地"
     };
     let intro = heroPageData.intro.split(", ");
-
-    let portfolioItems = [];
-    for (let i = 0; i < 4; i++) {
-      portfolioItems.push(require("../images/logo.jpg"));
-    }
-
-    return [
+    return (
       <div
         key={1}
         id="hero-img-container"
@@ -25,13 +25,18 @@ class HeroPage extends React.Component {
             return <h2 key={i}>{item}</h2>;
           })}
         </div>
-      </div>,
-      <div key={2} className="container">
-        <div className="container-banner">
-          <h1>About</h1>
-        </div>
-        <span className="col-6 container-box">
-          <div id="about-img-div">
+      </div>
+    );
+  }
+}
+
+class HeroPage_About extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="container-banner">About</div>
+        <div className="container-box">
+          <div className="col-6" id="about-img-div">
             <img
               id="about-img"
               className="circle-image"
@@ -40,19 +45,27 @@ class HeroPage extends React.Component {
               onClick={() => this.props.renderBody(<HeroPage />)}
             />
           </div>
-        </span>
-        <span className="col-6 container-box">
-          <div id="about-intro-div">
+          <div className="col-6" id="about-intro-div">
             yoyoyo<br />
             <br /> hehehehehehehe<br />ehehehxhxhskal<br />a1234slslslllsl<br />s234u7uf9a8h9<br
             />j32897vajho9dshf
           </div>
-        </span>
-      </div>,
-      <div key={3} className="container" id="heropage-portfolio">
-        <div className="container-banner">
-          <h1>Portfolio</h1>
         </div>
+        <div className="container-footer">more...</div>
+      </div>
+    );
+  }
+}
+
+class HeroPage_Portfolio extends React.Component {
+  render() {
+    let portfolioItems = [];
+    for (let i = 0; i < 4; i++) {
+      portfolioItems.push(require("../images/logo.jpg"));
+    }
+    return (
+      <div key={3} className="container" id="heropage-portfolio">
+        <div className="container-banner">Portfolio</div>
         <div className="col-12 container-box">
           {portfolioItems.map((item, i) => {
             return (
@@ -68,8 +81,9 @@ class HeroPage extends React.Component {
             );
           })}
         </div>
+        <div className="container-footer">more...</div>
       </div>
-    ];
+    );
   }
 }
 
